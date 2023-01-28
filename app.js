d59a268819cd1c0d968fb16f223def946d81fceb
Wagner Santiago <66319302+wagnert3@users.noreply.github.com>
2023-01-28 16:54:35 -0400

    EX4 Completed

---
+++ app.js
@@ -0,0 +1,5 @@
+let helloFromExternalJS = 'Hello World!';
+console.log(helloFromExternalJS)
+
+//alert(helloFromExternalJS); // only available when browser compiles, is not a node interface
+let x = prompt('test');
